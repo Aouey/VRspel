65ea2a0ab766c63af52fe9f4491ee12600a75a8f
Henry <henryandersson5@gmail.com>
2020-03-18 09:37:27 +0100

    inte snopp

---
+++ Assets/Scripts/evElse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class evElse : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case ("BlueKey"):
+                PickUpKey(other);
+                break;
+            case ("RedKey"):
+                PickUpKey(other);
+                break;
+        }
+    }
+
+    void PickUpKey(Collider Col)
+    {
+        Destroy(Col.gameObject);
+    }
+}
