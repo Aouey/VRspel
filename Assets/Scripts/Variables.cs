b7b3dab78797cfeedf063d59b4e42b2318fadc4c
Elin <elin.forsberg@edu.alleksolan.eu>
2020-03-18 10:06:21 +0100

    hello

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -21,7 +21,7 @@ public class Variables : MonoBehaviour
             HasBlueKey = !HasBlueKey;
         }
 
-        if ()
+        if (Red == true)
         {
             HasRedKey = !HasRedKey;
         }
