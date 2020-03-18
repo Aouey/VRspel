6b67eea9b5d6de23b4de17a093284519fcbeeac1
Henry <henryandersson5@gmail.com>
2020-03-18 10:24:01 +0100

    mina saker

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -28,4 +28,5 @@ public class Variables : MonoBehaviour
 
     }
 
+
 }
--- Assets/Scripts/evElse.cs
+++ Assets/Scripts/evElse.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class evElse : MonoBehaviour
 {
+
+    public RawImage RedKey;
+    public RawImage BlueKey;
+
+    private void Awake()
+    {
+        RedKey.enabled = false;
+        BlueKey.enabled = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
         switch (other.gameObject.tag)
         {
             case ("BlueKey"):
-                PickUpKey(other);
+                PickUpKey(other, BlueKey);
                 break;
             case ("RedKey"):
-                PickUpKey(other);
+                PickUpKey(other, RedKey);
                 break;
         }
     }
 
-    void PickUpKey(Collider Col)
+    void PickUpKey(Collider Col, RawImage key)
     {
         Destroy(Col.gameObject);
+        key.enabled = true;
     }
 }
