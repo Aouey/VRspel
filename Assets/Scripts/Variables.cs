0b718a225fed0ba3e0d1ba683156c5c19c00de84
Elin <elin.forsberg@edu.alleksolan.eu>
2020-03-18 10:02:22 +0100

    Auto stash before merge of "master" and "origin/master"
    hello

---
+++ Assets/Scripts/Variables.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    bool HasRedKey;
+    bool HasBlueKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HasRedKey = false;
+        HasBlueKey = false;
+    }
+
+    public void ChangeKey(bool Blue, bool Red)
+    {
+        if (Blue == true)
+        {
+            HasBlueKey = !HasBlueKey;
+        }
+
+        if ()
+        {
+            HasRedKey = !HasRedKey;
+        }
+
+    }
+
+}
